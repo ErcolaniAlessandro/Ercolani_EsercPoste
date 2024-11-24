6640fc391e62b6ad2888b828a54be9ceafd9c0c8
ErcolaniAlessandro <alessandroercolani22@gmail.com>
2024-11-24 15:12:52 +0100

    Update Sportello.java

--- src/main/java/com/mycompany/mainposta/Sportello.java
+++ src/main/java/com/mycompany/mainposta/Sportello.java
@@ -10,19 +10,22 @@ package com.mycompany.mainposta;
  */
 public class Sportello {
     private ListaClienti listaClienti;
-    private final int minTempoServizio=2000;
-    private final int maxTempoServizio=8000;
-    public Sportello(ListaClienti listaClienti){
+    private final int minTempoServizio=6000;
+    private final int maxTempoServizio=12000;
+    private int numeroSportello;
+    
+    public Sportello(ListaClienti listaClienti, int numeroSportello){
         this.listaClienti=listaClienti;
+        this.numeroSportello=numeroSportello;
     }
     public void run(){
         try{
             while(!Thread.interrupted()){
                 Integer clienteServito = listaClienti.rimuoviCliente();
-                System.out.println("Chiamato Cliente Numero \t" + clienteServito);
+                System.out.println("Chiamato Cliente Numero \t" + clienteServito+"dallo sportello nuemro" + numeroSportello);
                 int tempoServizio = (int) (Math.random() * (maxTempoServizio-minTempoServizio + 1) + minTempoServizio);
                 Thread.sleep(tempoServizio);
-                System.out.println("Servito Cliente Numero \t"+clienteServito);
+                System.out.println("Servito Cliente Numero \t"+clienteServito+ clienteServito + "dallo sportello numero" + numeroSportello);
             }
         }catch(InterruptedException e){
              System.out.println("Tread interrotto durante lo sleep");
